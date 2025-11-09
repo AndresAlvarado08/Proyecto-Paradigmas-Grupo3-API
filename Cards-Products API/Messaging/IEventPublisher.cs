2f091c9816dac1226fc7fdc4d8437fcebe0907bc
Alondra Flores <alondraespinoza897@gmail.com>
2025-11-09 17:16:44 -0600

    Revert "Merge remote-tracking branch 'origin/Ronaldo-features' into Alondra-features"
    This reverts commit a13237394ef14a6b697cdb8445bae5ec12e9b8f9, reversing
    changes made to 45b5ff3d60b169b3e53856c75526f0ba760b3cb4.

--- Cards-Products API/Messaging/IEventPublisher.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace Cards_Products_API.Messaging
-{
-    public interface IEventPublisher
-    {
-        Task PublishOrderCreatedAsync(object payload);
-    }
-}
